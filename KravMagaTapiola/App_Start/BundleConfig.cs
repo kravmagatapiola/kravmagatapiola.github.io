ee506942c2048e678848521676e1ba5273ced2a6
Teemu Qvick <teemu.qvick@gmail.com>
2016-08-13 21:52:11 +0300

    improve bundling

--- KravMagaTapiola/App_Start/BundleConfig.cs
+++ KravMagaTapiola/App_Start/BundleConfig.cs
@@ -7,9 +7,12 @@ namespace KravMagaTapiola
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
-
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include(
+                "~/Scripts/jquery-{version}.js")
+                .Include(
+                "~/Scripts/modernizr-*"));
+            
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                 "~/Scripts/jquery.unobtrusive*",
                 "~/Scripts/jquery.validate*"));
@@ -26,24 +29,19 @@ namespace KravMagaTapiola
                 "~/Scripts/app/home.viewmodel.js",
                 "~/Scripts/app/_run.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new ScriptBundle("~/Scripts/site")
+                .Include(
                 "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/Scripts/site").Include(
-            "~/Scripts/plugins/isotope/isotope.pkgd.min.js",
-            "~/Scripts/plugins/jquery.backstretch.min.js", 
-            "~/Scripts/plugins/jquery.appear.js", 
-            "~/Scripts/plugins/simplePlayer.js", 
-            "~/Scripts/plugins/googlecal.js",
-            "~/Scripts/plugins/light-gallery/lightGallery.js",
-            "~/Scripts/template.js", 
-            "~/Scripts/custom.js"));
+                "~/Scripts/respond.js")
+                .Include(
+                "~/Scripts/plugins/isotope/isotope.pkgd.min.js",
+                "~/Scripts/plugins/jquery.backstretch.min.js", 
+                "~/Scripts/plugins/jquery.appear.js", 
+                "~/Scripts/plugins/simplePlayer.js", 
+                "~/Scripts/plugins/googlecal.js",
+                "~/Scripts/plugins/light-gallery/lightGallery.js",
+                "~/Scripts/template.js", 
+                "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                  "~/Content/bootstrap.css",
