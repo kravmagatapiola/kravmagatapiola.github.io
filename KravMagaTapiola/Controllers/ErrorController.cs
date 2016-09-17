81427896caa61505e1af8acc1ec055bd60624bca
Teemu Qvick <teemu.qvick@gmail.com>
2016-09-17 22:24:40 +0300

    add intro courses

---
+++ KravMagaTapiola/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace KravMagaTapiola.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public RedirectResult Index()
+        {
+            return RedirectPermanent(Url.Action("Index", "Home"));
+        }
+
+        public RedirectResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return RedirectPermanent(Url.Action("Index", "Home"));
+        }
+    }
+}
