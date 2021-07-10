7d1b5f3a38c4e059a141c84bcc73792001758313
Teemu Qvick <teemu.qvick@finbiosoft.com>
2021-07-10 21:57:11 +0300

    Fix myclub embed code

--- js/template.js
+++ js/template.js
@@ -190,13 +190,5 @@ window.onload = init;
 ga("create", "UA-66211929-1", "auto");
 ga("send", "pageview");
 
-(function (d, s, id) {
-    var js;
-    const mjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.async = true;
-    js.src = "https://kravmaga.myclub.fi/assets/embed_v2.js";
-    mjs.parentNode.insertBefore(js, mjs);
-}(document, "script", "myclub-embed-js"));
+function(d, s, id) { var js, mjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) return; js = d.createElement(s); js.id = id; js.async = true; js.src = "https://d2ubdm6yoox6gh.cloudfront.net/assets/myclub-embed.js"; mjs.parentNode.insertBefore(js, mjs); }(document, 'script', 'myclub-embed-js'));
+			
